757c7e908023b35c511a1e9ab79d5c66c2d6753e
HiroMuraki <419734782@qq.com>
2020-08-20 16:08:05 +0800

    add a script file to compile in one click and copy to the desktop

---
+++ DistributeGameFile.ps1
@@ -0,0 +1,4 @@
+MSBuild "/p:Configuration=Release;Optimize=true" "/t:rebuild"
+copy "GridGameHOS\bin\Release\GridGameHOS.exe" "$env:userprofile\Desktop\GridGameHOS.exe"
+ren "$env:userprofile\Desktop\GridGameHOS.exe" "É¨À×HOS.exe"
+pause

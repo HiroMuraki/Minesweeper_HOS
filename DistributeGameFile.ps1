0dbb516f9dc0edc9f6c82ab82bfacd9b19ed366f
HiroMuraki <419734782@qq.com>
2020-08-20 18:59:53 +0800

    update the quick compile script

--- DistributeGameFile.ps1
+++ DistributeGameFile.ps1
@@ -1,4 +1,5 @@
 MSBuild "/p:Configuration=Release;Optimize=true" "/t:rebuild"
+del "$env:userprofile\Desktop\É¨À×HOS.exe"
 copy "GridGameHOS\bin\Release\GridGameHOS.exe" "$env:userprofile\Desktop\GridGameHOS.exe"
 ren "$env:userprofile\Desktop\GridGameHOS.exe" "É¨À×HOS.exe"
 pause
